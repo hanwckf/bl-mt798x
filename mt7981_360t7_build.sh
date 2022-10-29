fb966e7951f7310be4ef20dfc2f426780f1d85d5
hanwckf <hanwckf@vip.qq.com>
2022-10-29 14:10:44 +0800

    add "fixedparts" param to build fip with fixed-parts

--- mt7981_360t7_build.sh
+++ mt7981_360t7_build.sh
@@ -15,7 +15,12 @@ ATF_DIR=atf-20220606-637ba581b
 ATF_CFG=mt7981_360t7_defconfig
 
 echo "build u-boot..."
+
 cp -f $UBOOT_DIR/configs/$UBOOT_CFG $UBOOT_DIR/.config
+if [ "$1" = "fixedparts" ]; then
+	echo "CONFIG_MEDIATEK_UBI_FIXED_MTDPARTS=y" >> $UBOOT_DIR/.config
+fi
+
 make -C $UBOOT_DIR olddefconfig all
 if [ -f "$UBOOT_DIR/u-boot.bin" ]; then
 	cp -f $UBOOT_DIR/u-boot.bin $ATF_DIR/u-boot.bin
@@ -26,7 +31,7 @@ else
 fi
 
 echo "build atf..."
-make -C $ATF_DIR -f makefile $ATF_CFG all
+make -C $ATF_DIR -f makefile $ATF_CFG all CONFIG_CROSS_COMPILER=${TOOLCHAIN}
 if [ -f "$ATF_DIR/build/mt7981/release/fip.bin" ]; then
 	cp -f $ATF_DIR/build/mt7981/release/fip.bin $BOARD-fip.bin
 	echo "fip.bin done"
