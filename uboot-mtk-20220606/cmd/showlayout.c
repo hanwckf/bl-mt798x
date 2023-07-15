0608830f8b392ee3ba47e87f6a66a51dc24ec6c5
hanwckf <hanwckf@vip.qq.com>
2023-07-15 20:58:36 +0800

    cmd: add "showlayout" command to get available mtd layouts in fdt

---
+++ uboot-mtk-20220606/cmd/showlayout.c
@@ -0,0 +1,30 @@
+#include <common.h>
+#include <command.h>
+#include <dm/ofnode.h>
+
+
+static int do_showlayout(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[])
+{
+    ofnode node, layout;
+
+    node = ofnode_path("/mtd-layout");
+
+    if (ofnode_valid(node) && ofnode_get_child_count(node)) {
+        ofnode_for_each_subnode(layout, node) {
+            printf("mtd label: %s, mtdids: %s, mtdparts: %s\n", 
+                ofnode_read_string(layout, "label"), 
+                ofnode_read_string(layout, "mtdids"), 
+                ofnode_read_string(layout, "mtdparts"));
+        }
+    } else {
+        printf("get mtd layout failed!\n");
+    }
+    
+    return CMD_RET_SUCCESS;
+}
+
+U_BOOT_CMD(
+	showlayout, 2, 0, do_showlayout,
+	"Show mtd layout",
+	""
+);
