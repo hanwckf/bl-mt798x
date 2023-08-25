b1a99646ff469e8ca35a727813618f55e0d100b2
benboguan <nanchuci023@gmail.com>
2023-08-25 23:38:47 +0800

    r30b1:add clt-r30b1 actions support

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ ATF_DIR=atf-20220606-637ba581b
 if [ -z "$SOC" ] || [ -z "$BOARD" ]; then
 	echo "Usage: SOC=[mt7981|mt7986] BOARD=<board name> FIXED_MTDPARTS=[1|0] MULTI_LAYOUT=[0|1] $0"
 	echo "eg: SOC=mt7981 BOARD=360t7 $0"
-	echo "eg: SOC=mt7981 BOARD=wr30u MULTI_LAYOUT=1 $0"
+	echo "eg: SOC=mt7981 BOARD=clt_r30b1 MULTI_LAYOUT=1 $0"
 	echo "eg: SOC=mt7986 BOARD=redmi_ax6000 $0"
 	exit 1
 fi
@@ -21,9 +21,9 @@ command -v ${TOOLCHAIN}gcc
 [ "$?" != "0" ] && { echo ${TOOLCHAIN}gcc not found!; exit 0; }
 export CROSS_COMPILE=$TOOLCHAIN
 
-# Build fixed-mtdparts by default
+# Build fixed-mtdparts and multi-layout by default
 fixedparts=${FIXED_MTDPARTS:-1}
-multilayout=${MULTI_LAYOUT:-0}
+multilayout=${MULTI_LAYOUT:-1}
 
 echo "Building for CPU: $SOC, BOARD: $BOARD, fixed-mtdparts: $fixedparts, multi-layout: $multilayout"
 
@@ -65,14 +65,12 @@ else
 	make -C $ATF_DIR -f makefile all CONFIG_CROSS_COMPILER=${TOOLCHAIN}
 	if [ -f "$ATF_DIR/build/${SOC}/release/fip.bin" ]; then
 		mkdir -p output
-		FIP_NAME="${SOC}_${BOARD}-fip"
-		if [ "$fixedparts" = "1" ]; then
-			FIP_NAME="${FIP_NAME}-fixed-parts"
+		if [ "$multilayout" = "1" ] && [ "$fixedparts" = "1" ]; then
+			FIP_NAME="${SOC}_${BOARD}-fip-fixed-parts.bin"
+		else
+			FIP_NAME="${SOC}_${BOARD}-fip.bin"
 		fi
-		if [ "$multilayout" = "1" ]; then
-			FIP_NAME="${FIP_NAME}-multi-layout"
-		fi
-		cp -f $ATF_DIR/build/${SOC}/release/fip.bin output/${FIP_NAME}.bin
+		cp -f $ATF_DIR/build/${SOC}/release/fip.bin output/${FIP_NAME}
 		echo "$FIP_NAME build done"
 	else
 		echo "fip build fail!"
