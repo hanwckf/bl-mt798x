e616bb14f6fee2fbad0b2f44976a9d1e6605e9cf
Tianling Shen <cnsztl@immortalwrt.org>
2024-09-08 00:35:38 +0800

    uboot-mtk-20220606: fix auto enter failsafe on autoboot failure
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- uboot-mtk-20220606/board/mediatek/common/cmd_mtkboardboot.c
+++ uboot-mtk-20220606/board/mediatek/common/cmd_mtkboardboot.c
@@ -15,13 +15,16 @@
 static int do_mtkboardboot(struct cmd_tbl *cmdtp, int flag, int argc,
 			   char *const argv[])
 {
-	int ret;
+	int ret = CMD_RET_SUCCESS;
 
 	ret = board_boot_default();
 	if (ret)
-		return CMD_RET_FAILURE;
+		ret = CMD_RET_FAILURE;
 
-	return CMD_RET_SUCCESS;
+	if (IS_ENABLED(CONFIG_WEBUI_FAILSAFE_ON_AUTOBOOT_FAIL))
+		run_command("httpd", 0);
+
+	return ret;
 }
 
 U_BOOT_CMD(mtkboardboot, 1, 0, do_mtkboardboot,
