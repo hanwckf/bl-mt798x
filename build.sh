77e59e402e9906019776577da02ccd14569a2ac3
Tianling Shen <cnsztl@immortalwrt.org>
2025-02-19 02:50:27 +0800

    build.sh: support new menuconfig
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- build.sh
+++ build.sh
@@ -59,6 +59,7 @@ if [ "$fixedparts" = "1" ]; then
 	echo "CONFIG_MTK_FIXED_MTD_MTDPARTS=y" >> "$UBOOT_DIR/.config"
 fi
 make -C "$UBOOT_DIR" olddefconfig
+make -C "$UBOOT_DIR" clean
 make -C "$UBOOT_DIR" -j $(nproc) all
 if [ -f "$UBOOT_DIR/u-boot.bin" ]; then
 	cp -f "$UBOOT_DIR/u-boot.bin" "$ATF_DIR/u-boot.bin"
@@ -69,13 +70,18 @@ else
 fi
 
 echo "Build atf..."
-make -C "$ATF_DIR" -f makefile "$ATF_CFG" CONFIG_CROSS_COMPILER="${TOOLCHAIN}"
-make -C "$ATF_DIR" -f makefile clean CONFIG_CROSS_COMPILER="${TOOLCHAIN}"
+if [ -e "$ATF_DIR/makefile" ]; then
+	ATF_MKFILE="makefile"
+else
+	ATF_MKFILE="Makefile"
+fi
+make -C "$ATF_DIR" -f "$ATF_MKFILE" clean CONFIG_CROSS_COMPILER="$TOOLCHAIN" CROSS_COMPILER="$TOOLCHAIN"
 rm -rf "$ATF_DIR/build"
-make -C "$ATF_DIR" -f makefile all CONFIG_CROSS_COMPILER="${TOOLCHAIN}"
+make -C "$ATF_DIR" -f "$ATF_MKFILE" "$ATF_CFG" CONFIG_CROSS_COMPILER="$TOOLCHAIN" CROSS_COMPILER="$TOOLCHAIN"
+make -C "$ATF_DIR" -f "$ATF_MKFILE" all CONFIG_CROSS_COMPILER="$TOOLCHAIN" CROSS_COMPILER="$TOOLCHAIN" -j $(nproc)
 
 mkdir -p "output"
-if [ -f "$ATF_DIR/build/${SOC}/release/fip.bin" ]; then
+if [ -f "$ATF_DIR/build/$SOC/release/fip.bin" ]; then
 	FIP_NAME="${SOC}_${BOARD}-fip"
 	if [ "$fixedparts" = "1" ]; then
 		FIP_NAME="${FIP_NAME}-fixed-parts"
@@ -83,16 +89,16 @@ if [ -f "$ATF_DIR/build/${SOC}/release/fip.bin" ]; then
 	if [ "$multilayout" = "1" ]; then
 		FIP_NAME="${FIP_NAME}-multi-layout"
 	fi
-	cp -f "$ATF_DIR/build/${SOC}/release/fip.bin" "output/${FIP_NAME}.bin"
+	cp -f "$ATF_DIR/build/$SOC/release/fip.bin" "output/$FIP_NAME.bin"
 	echo "$FIP_NAME build done"
 else
 	echo "fip build fail!"
 	exit 1
 fi
-if grep -q "CONFIG_TARGET_ALL_NO_SEC_BOOT=y" "$ATF_DIR/configs/$ATF_CFG"; then
-	if [ -f "$ATF_DIR/build/${SOC}/release/bl2.img" ]; then
+if grep -Eq "(^_|CONFIG_TARGET_ALL_NO_SEC_BOOT=y)" "$ATF_DIR/configs/$ATF_CFG"; then
+	if [ -f "$ATF_DIR/build/$SOC/release/bl2.img" ]; then
 		BL2_NAME="${SOC}_${BOARD}-bl2"
-		cp -f "$ATF_DIR/build/${SOC}/release/bl2.img" "output/${BL2_NAME}.bin"
+		cp -f "$ATF_DIR/build/$SOC/release/bl2.img" "output/$BL2_NAME.bin"
 		echo "$BL2_NAME build done"
 	else
 		echo "bl2 build fail!"
